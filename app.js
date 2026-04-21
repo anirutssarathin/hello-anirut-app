e0692754ceddb012bd2508c9c94f27d87cfe37f7
unknown <anirut.rstar@gmail.com>
2026-04-21 22:10:12 +0700

    initial commit: express + docker + cicd

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello Anirut!</h1><p>CI/CD with Docker is running successfully.</p>');
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
