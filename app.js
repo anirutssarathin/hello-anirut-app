69108572aab273416aa63e54b737c3cf4267be8d
unknown <anirut.rstar@gmail.com>
2026-04-21 22:57:49 +0700

    fix: use docker-compose with hyphen

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello Anirut!111</h1><p>CI/CD with Docker is running successfully.</p>');
+  res.send('<h1>Hello Anirut!111</h1><p>CI/CD with Docker12 is running successfully.</p>');
 });
 
 app.listen(port, '0.0.0.0', () => {
